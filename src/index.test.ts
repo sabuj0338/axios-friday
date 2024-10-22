63d99ec1e35af9f817d4a6f27815e419a5c62382
Sabuj Islam <sabuj0338@gmail.com>
2024-10-22 13:09:14 +0600

    axios-friday renamed

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,20 +1,13 @@
 import { describe, expect, it } from "vitest";
-import BorakApiClient from "./index";
+import Friday from "./index";
 
-describe('GET /', () => {
+describe("GET /", () => {
+  const baseURL = "https://sabuj0338.github.io/portfolio";
 
-  const baseURL = "https://hundred-api.vercel.app";
+  const friday = new Friday({ baseURL: baseURL });
 
-  const borak = new BorakApiClient({
-    baseURL: baseURL,
-    accessTokenKey: "accessToken",
-    refreshTokenKey: "refreshToken",
-    refreshTokenAPI: "/api/refresh",
-    enableRefreshToken: false,
-  });
-
-  it('should return a Hello World! message', async () => {
-    const response = await borak.get(new URL(baseURL));
-    expect(response).toBe("Hello World!");
+  it("should return a Hello World! message", async () => {
+    const response = await friday.get(new URL(baseURL));
+    expect(response?.status).toBe(200);
   });
 });
